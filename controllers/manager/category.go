9ee72a5b49841b0275622778d25726235792ed90
ruoL <zhangvipqi@163.com>
2015-11-18 18:37:49 +0800

    last

--- controllers/manager/category.go
+++ controllers/manager/category.go
@@ -1,12 +1,46 @@
 package manager
 
+import (
+	"code/models"
+	"github.com/astaxie/beego"
+)
+
 type CategoryController struct {
 	BaseController
 }
 
-func (this *CategoryController) Get() {}
+func (this *CategoryController) Get() {
+	this.TplNames = "manager/category_index.html"
+	var err error
+	this.Data["Categories"], err = models.GetCategories("0", "10")
+	if err != nil {
+		beego.Error(err)
+	}
+}
 
-func (this *CategoryController) Create() {}
+func (this *CategoryController) Create() {
+	if this.Ctx.Input.Method() == "GET" {
+		this.TplNames = "manager/category_create.html"
+		this.Layout = "manager/layout.html"
+		return
+	}
+
+	name := this.GetString("name")
+	if name == "" {
+		this.Data["json"] = map[string]string{"code": "error", "info": "分类标题不能为空！"}
+		this.ServeJson()
+		return
+	}
+
+	err := models.AddCategory(name)
+	if err != nil {
+		this.Data["json"] = map[string]string{"code": "error", "info": err.Error()}
+	} else {
+		this.Data["json"] = map[string]string{"code": "success", "info": "分类添加成功！"}
+	}
+	this.ServeJson()
+	return
+}
 
 func (this *CategoryController) Delete() {}
 
